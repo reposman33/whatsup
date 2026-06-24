1706096cf490c73a6e78613e95186a1b36604a9f
Marc <webdev@marcbakker.com>
2026-06-24 17:25:19 +0200

    fix typo in catchall route waardoor niet alles gevangen werd

--- apps/whatsup/src/app/app.routes.ts
+++ apps/whatsup/src/app/app.routes.ts
@@ -5,5 +5,5 @@ import { authenticationGuard } from './shared/guards/authentication-guard';
 export const appRoutes: Route[] = [
   {path: 'login', loadComponent: () => import('./components/login/login').then(m => m.Login)},
   {path: 'whatsUp', loadComponent: () => import('./components/whats-up/whats-up').then(m => m.WhatsUp), canActivate: [authenticationGuard]},
-  {path: '*', redirectTo: 'whatsUp'}
+  {path: '**', redirectTo: 'whatsUp'}
 ];
