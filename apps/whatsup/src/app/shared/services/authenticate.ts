bb80cfe954ef2f3ea0989745d008cd6967d13506
Marc <webdev@marcbakker.com>
2026-06-24 18:08:16 +0200

    implemeteer register en authenticate methods via localstorage.

--- apps/whatsup/src/app/shared/services/authenticate.ts
+++ apps/whatsup/src/app/shared/services/authenticate.ts
@@ -1,14 +1,38 @@
 import { Injectable } from '@angular/core';
 
+type loginMethod = 'localstorage' | 'firebase';
 @Injectable({
   providedIn: 'root',
 })
+
 export class AuthenticateService {
+  private loginMethod: loginMethod = 'localstorage';
+  // firebase
+  // ...
+
   private authenticated = false
   private _requestedUrl:string | undefined = undefined;
 
+  register(email: string, password: string) {
+    switch (this.loginMethod) {
+    case 'localstorage':
+      this.addUserToLocalStorage(email, password);
+    }
+  }
+
+  addUserToLocalStorage(email: string, password: string) {
+    const registeredUsers = JSON.parse(localStorage.getItem('registeredUsers') || '[]');
+    registeredUsers.push({ email, password });
+    
+    localStorage.setItem('registeredUsers', JSON.stringify(registeredUsers));
+  }
+
   authenticate(email: string, password: string) {
-    this.authenticated = email === 'test@test.com' && password === 'test';
+    // Implementation for authentication
+    switch (this.loginMethod) {
+      case 'localstorage':
+        this.authenticateWithLocalStorage(email, password);
+      }
   }
 
   isAuthenticated() {
@@ -22,5 +46,12 @@ export class AuthenticateService {
   get requestedUrl(): string | undefined {
     return this._requestedUrl;
   }
+
+  authenticateWithLocalStorage(email: string, password: string) {
+    const registeredUsers = JSON.parse(localStorage.getItem('registeredUsers') || '[]');
+    const user = registeredUsers.find((user: { email: string; password: string }) => user.email === email && user.password === password);
+    
+    this.authenticated = user !== undefined;
+   }
 
 }
