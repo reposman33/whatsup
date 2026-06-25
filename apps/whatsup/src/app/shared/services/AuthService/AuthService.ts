6377e6728b65ee5e88e697cfecedf37cfb551556
Marc <webdev@marcbakker.com>
2026-06-25 16:23:12 +0200

    Gebruik Observable i.p.v. method isAuthenticated() om authenticatiestatus bij te houden.

--- apps/whatsup/src/app/shared/services/AuthService/AuthService.ts
+++ apps/whatsup/src/app/shared/services/AuthService/AuthService.ts
@@ -13,7 +13,6 @@ export class AuthService {
   authenticated$ = this.authenticatedSubject.asObservable();
   apiService = inject(ApiService);
 
-  private authenticated = false
   private _requestedUrl:string | undefined = undefined;
 
   register(email: string, password: string, gebruikersNaam: string) {
@@ -22,12 +21,7 @@ export class AuthService {
 
   authenticate(email: string, password: string) {
     const user = this.apiService.getUser(email,password)
-    this.authenticated = !!user;
     this.authenticatedSubject.next(!!user);
-  }
-
-   isAuthenticated() {
-    return this.authenticated
   }
 
   set requestedUrl(url: string | undefined) {
@@ -39,7 +33,6 @@ export class AuthService {
   }
 
    logout() {
-    this.authenticated = false;
     this.authenticatedSubject.next(false);
    }
 }
