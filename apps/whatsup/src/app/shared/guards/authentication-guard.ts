7f364776b7c43d0dac07acb202a29113d6e08be0
Marc <webdev@marcbakker.com>
2026-06-25 15:25:34 +0200

    subscribe op AuthService.authenticated$ i.p.v. method isAuthenticated() aanroepen

--- apps/whatsup/src/app/shared/guards/authentication-guard.ts
+++ apps/whatsup/src/app/shared/guards/authentication-guard.ts
@@ -1,15 +1,16 @@
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '@services/AuthService/AuthService';
 import { inject } from '@angular/core';
+import { map } from 'rxjs';
 
 export const authenticationGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
   const router = inject(Router);
 
-  if(authService.isAuthenticated()) {
-    return true
-  }
-
-  authService.requestedUrl = state.url;
-  return router.createUrlTree(['login']);
+  return authService.authenticated$.pipe(
+    map(isAuthenticated => 
+      isAuthenticated ?
+      true :
+      router.createUrlTree(['login']))
+  )
 };
