bb6706df3a58265e06b5b70dd29cb5fbeffbad03
Marc <webdev@marcbakker.com>
2026-06-29 14:43:15 +0200

    refactor: verwijder ongebruikte AsyncPipe

--- apps/whatsup/src/app/components/header/header.ts
+++ apps/whatsup/src/app/components/header/header.ts
@@ -1,11 +1,10 @@
 import { ChangeDetectionStrategy, Component, inject, ViewEncapsulation } from '@angular/core';
 import { AuthService } from '@services/AuthService/AuthService';
 import { Router } from '@angular/router';
-import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'header',
-  imports: [AsyncPipe],
+  imports: [],
   templateUrl: './header.html',
   styleUrl: './header.scss',
   encapsulation: ViewEncapsulation.Emulated,
