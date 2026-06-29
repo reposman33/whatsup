3c601c09c0cb420582ab74a69503fd49e573af30
Marc <webdev@marcbakker.com>
2026-06-29 15:58:52 +0200

    refactor: hernoem local-storage => local-storage.service

--- apps/whatsup/src/app/shared/services/index.ts
+++ apps/whatsup/src/app/shared/services/index.ts
@@ -1,3 +1,5 @@
 export * from './auth.service/auth.service';
 export * from './api.service/api.service';
 export * from './chat.service/chat.service';
+export * from './local-storage.service/local-storage.service';
+export * from './fire-base.service/fire-base.service';
---
+++ apps/whatsup/src/app/shared/services/local-storage.service/local-storage.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { Contact } from '@models/contact';
+import { Message } from '@models/message';
+import { isContact } from '@typeGuards/*';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class StorageService {
+
+  getConversationsWithContact(currentContactRegistrationTime: number, selectedContactRegistrationTime: number): Message[]{
+    return JSON.parse(localStorage.getItem('messages') || '[]')
+    .filter((m: Message) => m.sender === currentContactRegistrationTime && m.receiver === selectedContactRegistrationTime)
+  }
+
+  addContact(contact: Contact) {
+    const registeredContacts = JSON.parse(localStorage.getItem('registeredUsers') || '[]');
+    registeredContacts.push(contact);
+  
+    localStorage.setItem('registeredUsers', JSON.stringify(registeredContacts));
+  }
+
+  addMessage(message: Message){
+    const messages = JSON.parse(localStorage.getItem('messages') || '[]');
+    messages.push( message );
+    localStorage.setItem('messages', JSON.stringify(messages));
+  }
+
+  /**
+   * 
+   * @param email 
+   * @param password 
+   * @returns - een Contact object of undefined. Check op type Contact met een typeGuard
+   */
+  getContact(email: string, password: string): Contact | undefined {
+    const contact = JSON.parse(localStorage.getItem('registeredUsers') || '[]')
+    .find((contact: Contact) => contact.email === email && contact.password === password);
+    if(contact) {
+      // check met TypeGuard of dit echt een valide Contact object is
+      return isContact(contact) ? contact : undefined
+    }
+    return undefined
+  }
+  
+}
