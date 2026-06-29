c6519ddf0afc0a1fb528d4709ef2c15fbd2c5c0c
Marc <webdev@marcbakker.com>
2026-06-29 16:03:09 +0200

    Revert "refactor: hernoem local-storage => local-storage.service" nog niet klaar This reverts commit 3c601c09c0cb420582ab74a69503fd49e573af30.

--- apps/whatsup/src/app/shared/services/index.ts
+++ apps/whatsup/src/app/shared/services/index.ts
@@ -1,5 +1,3 @@
 export * from './auth.service/auth.service';
 export * from './api.service/api.service';
 export * from './chat.service/chat.service';
-export * from './local-storage.service/local-storage.service';
-export * from './fire-base.service/fire-base.service';
