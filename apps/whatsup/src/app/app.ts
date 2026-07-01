17edfe0c087179428d8b15eba38627215d290be7
Marc <webdev@marcbakker.com>
2026-07-01 18:29:46 +0200

    refactor: header is verplaatst naar MainLayoutComponent

--- apps/whatsup/src/app/app.ts
+++ apps/whatsup/src/app/app.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { Header } from './components/header/header';
 
 @Component({
-  imports: [RouterModule, Header],
+  imports: [RouterModule],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
