e8a55bac5f2b2811bbb60d0922e3fa228dd87f90
Marc <webdev@marcbakker.com>
2026-07-08 00:40:24 +0200

    maak getContacts() en getContact()

--- apps/whatsup/src/app/shared/services/api/firebase/fire-base.service.ts
+++ apps/whatsup/src/app/shared/services/api/firebase/fire-base.service.ts
@@ -1,25 +1,40 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { collection, collectionData, doc, getDoc, Firestore } from '@angular/fire/firestore';
 import { Contact } from '@models/contact';
 import { Message } from '@models/message';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FireBaseService {
 
-  getContact(email: string, password: string): Contact | undefined {
-    return undefined
+private firestore = inject(Firestore);
+
+async getContact(id: string): Promise<Contact | undefined> {
+    const docRef = doc(this.firestore, `contacts/${id}`);
+    const snap = await getDoc(docRef);
+    if (!snap.exists()) return undefined;
+    const data = snap.data() as Contact;
+
+    // attach id if needed
+    return { ...(data as Contact), id: (snap.id as any) } as Contact;
   }
 
-  getContacts(): Contact[] {
-    return []
+  getContacts(): Observable<Contact[]> {
+    const contactsRef = collection(this.firestore, 'contacts');
+    return collectionData(contactsRef, { idField: 'id' }) as Observable<Contact[]>;    
   }
 
-  registerContact(contact: Contact): void {}  
+  registerContact(contact: Contact): void {
+    throw new Error('Method not implemented.');
+  }
   
-  addMessage(message: Message): void {}
+  addMessage(message: Message): void {
+    throw new Error('Method not implemented.');
+  }
   
-    getMessagesWithContact(currentContactRegistrationTime: number, selectedContactRegistrationTime: number): Message[] {
-    return []
+  getMessagesWithContact(currentContactRegistrationTime: number, selectedContactRegistrationTime: number): Message[] {
+    throw new Error('Method not implemented.');
   }
 }
