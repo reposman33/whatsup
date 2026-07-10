97dd3cee6dd4d9832d88d6656788b17d28970bc4
Marc <webdev@marcbakker.com>
2026-07-10 15:07:34 +0200

    fix Vercel build error: "TS2339: Property 'firebaseConfig' does not exist on type '{ storageMethod: string;"

--- apps/whatsup/src/app/environment.ts
+++ apps/whatsup/src/app/environment.ts
@@ -1,3 +1,27 @@
-export const AppConfig = {
-  storageMethod: 'localStorage'
+const env = (import.meta as any).env;
+
+interface AppConfigType {
+  storageMethod: string;
+  firebaseConfig?: {
+    apiKey: string;
+    authDomain: string;
+    projectId: string;
+    storageBucket: string;
+    messagingSenderId: string;
+    appId: string;
+    measurementId: string;
+  };
+}
+
+export const AppConfig: AppConfigType = {
+  storageMethod: env.NG_APP_STORAGE_METHOD,
+  firebaseConfig: {
+    apiKey: env.NG_APP_FIREBASE_API_KEY,
+    authDomain: env.NG_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: env.NG_APP_FIREBASE_PROJECT_ID,
+    storageBucket: env.NG_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: env.NG_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: env.NG_APP_FIREBASE_APP_ID,
+    measurementId: env.NG_APP_FIREBASE_MEASUREMENT_ID,
+    }
 }
