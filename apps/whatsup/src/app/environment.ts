024ba06623094f3426280ab8612b62ee766f1d0c
Marc <webdev@marcbakker.com>
2026-07-10 16:23:17 +0200

    fix build error "TS2339: Property 'env' does not exist": verplaats env.d.ts naar ./apps/whatsup.src

--- apps/whatsup/src/app/environment.ts
+++ apps/whatsup/src/app/environment.ts
@@ -1,12 +1,3 @@
-const env = (import.meta as any).env;
-
-
-console.log('🔍 import.meta.env:', env);
-console.log('🔍 API key value:', env?.NG_APP_STORAGE_METHOD);
-console.log('🔍 API key value:', env.NG_APP_STORAGE_METHOD);
-console.log('🔍 API key value:', env?.NG_APP_FIREBASE_API_KEY);
-console.log('🔍 API key value:', env.NG_APP_FIREBASE_API_KEY);
-
 interface AppConfigType {
   storageMethod: string;
   firebaseConfig?: {
@@ -21,14 +12,14 @@ interface AppConfigType {
 }
 
 export const AppConfig: AppConfigType = {
-  storageMethod: env.NG_APP_STORAGE_METHOD,
+  storageMethod: import.meta.env.NG_APP_STORAGE_METHOD,
   firebaseConfig: {
-    apiKey: env.NG_APP_FIREBASE_API_KEY,
-    authDomain: env.NG_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: env.NG_APP_FIREBASE_PROJECT_ID,
-    storageBucket: env.NG_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: env.NG_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: env.NG_APP_FIREBASE_APP_ID,
-    measurementId: env.NG_APP_FIREBASE_MEASUREMENT_ID,
+    apiKey: import.meta.env.NG_APP_FIREBASE_API_KEY,
+    authDomain: import.meta.env.NG_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.NG_APP_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.NG_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.NG_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.NG_APP_FIREBASE_APP_ID,
+    measurementId: import.meta.env.NG_APP_FIREBASE_MEASUREMENT_ID,
     }
 }
---
+++ apps/whatsup/src/env.d.ts
@@ -0,0 +1,19 @@
+declare global {
+  interface ImportMetaEnv {
+  readonly NG_APP_STORAGE_METHOD: string;
+  readonly NG_APP_FIREBASE_API_KEY: string;
+  readonly NG_APP_FIREBASE_AUTH_DOMAIN: string;
+  readonly NG_APP_FIREBASE_PROJECT_ID: string;
+  readonly NG_APP_FIREBASE_STORAGE_BUCKET: string;
+  readonly NG_APP_FIREBASE_MESSAGING_SENDER_ID: string;
+  readonly NG_APP_FIREBASE_APP_ID: string;
+  readonly NG_APP_FIREBASE_MEASUREMENT_ID: string;
+  }
+
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
+
+}
+
+export {}
