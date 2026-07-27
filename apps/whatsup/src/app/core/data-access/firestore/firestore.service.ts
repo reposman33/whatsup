f8d22a578e1ac67bdf3c4e51f1392e74f6bf7af6
Marc <webdev@marcbakker.com>
2026-07-27 16:50:18 +0200

    alfabetize de methods

--- apps/whatsup/src/app/core/data-access/firestore/firestore.service.ts
+++ apps/whatsup/src/app/core/data-access/firestore/firestore.service.ts
@@ -15,36 +15,6 @@ export class FirestoreService implements StorageProvider{
 
   private firestore = inject(Firestore);
 
-  async getContact(id: string): Promise<Contact | undefined> {
-    const docRef = doc(this.firestore, `contacts/${id}`);
-    const snap = await getDoc(docRef);
-    if (!snap.exists()) return undefined;
-    const data = await snap.data() as Contact;
-
-    // attach id if needed
-    return { ...(data as Contact), id: (snap.id as string) } as Contact;
-  }
-
-  getContacts(): Observable<Contact[]> {
-    const contactsRef = collection(this.firestore, 'contacts');
-    return collectionData(contactsRef, { idField: 'id' }) as Observable<Contact[]>;    
-  }
-
-  getContactsByGroup(groupId: string): Observable<Contact[]> {
-    const q = query(collection(this.firestore, 'groups'), where("id", "==", groupId))
-    return collectionData(q, {idField: 'id'}) as Observable<Contact[]>
-  }
-
-  async addMessage(message: Message): Promise<void> {
-    const messagesCollection = collection(this.firestore, 'messages')
-    await addDoc(messagesCollection, message)
-  }
-  
-  getMessagesWithSelectedContact(id: string): Observable<Message[]> {
-    const q = query(collection(this.firestore, 'messages'), where("conversationId", "==", id), orderBy('timeStamp', 'asc'))
-    return collectionData (q, {idField: 'id'}) as Observable<Message[]>
-  }
-
   addGroup(group: Group & {invitedContactsEmails: string[], currentContactId: string}): Observable<AddGroupResult> {
     const groupCollectionRef = collection(this.firestore, 'groups')
     return from(addDoc(groupCollectionRef, {createdAt: group.createdAt, description: group.description, name: group.name}))
@@ -55,6 +25,7 @@ export class FirestoreService implements StorageProvider{
         const fromUserId = group.currentContactId;
 
         if (!group.invitedContactsEmails || group.invitedContactsEmails.length === 0) {
+          // er zijn geen users uitgenodigd om deel te nemen aan deze groep, return alleen de group
           return of({
             id: g.id, ...group,
             failedEmails: []
@@ -97,16 +68,59 @@ export class FirestoreService implements StorageProvider{
     );
   }
 
-  async getUserIdByEmailAddress(email:string): Promise<string | undefined> {
-    const q = query(collection(this.firestore,'contacts'), where('email','==',email))
-    const contacts = await firstValueFrom(collectionData(q, {idField: 'id'}))
+  async addMessage(message: Message): Promise<void> {
+    const messagesCollection = collection(this.firestore, 'messages')
+    await addDoc(messagesCollection, message)
+  }
+  
+  async getContact(id: string): Promise<Contact | undefined> {
+    const docRef = doc(this.firestore, `contacts/${id}`);
+    const snap = await getDoc(docRef);
+    if (!snap.exists()) return undefined;
+    const data = await snap.data() as Contact;
 
-    return contacts[0]?.id
+    // attach id if needed
+    return { ...(data as Contact), id: (snap.id as string) } as Contact;
+  }
+
+  getContactsByGroup(groupId: string): Observable<Contact[]> {
+    const membershipQuery = query(collection(this.firestore, 'memberships'), where("groupId", "==", groupId), where("status","==","accepted"));
+
+    return (collectionData(membershipQuery, {idField: 'id'}) as Observable<Membership[]>).pipe(
+      switchMap((memberships: Membership[]): Observable<Contact[]> => {
+        const userIds = memberships.map( (membership: Membership): string => membership.contactId);
+        
+        if(userIds.length === 0) {
+          return of([]) as Observable<Contact[]>
+        }
+
+        const contactsQuery = query(collection(this.firestore, 'contacts'), where(documentId(), "in", userIds))
+
+        return collectionData(contactsQuery, {idField: 'id'}) as Observable<Contact[]>
+      })
+    )
+  }
+
+  getContacts(): Observable<Contact[]> {
+    const contactsRef = collection(this.firestore, 'contacts');
+    return collectionData(contactsRef, { idField: 'id' }) as Observable<Contact[]>;    
   }
 
   getGroups(): Observable<Group[]> {
     const groupsRef = collection(this.firestore, 'groups');
     return collectionData(groupsRef, { idField: 'id' }) as Observable<Group[]>;    
+  }
+
+  getMessagesWithSelectedContact(id: string): Observable<Message[]> {
+    const q = query(collection(this.firestore, 'messages'), where("conversationId", "==", id), orderBy('timeStamp', 'asc'))
+    return collectionData (q, {idField: 'id'}) as Observable<Message[]>
+  }
+
+  async getUserIdByEmailAddress(email:string): Promise<string | undefined> {
+    const q = query(collection(this.firestore,'contacts'), where('email','==',email))
+    const contacts = await firstValueFrom(collectionData(q, {idField: 'id'}))
+
+    return contacts[0]?.id
   }
 
 }
