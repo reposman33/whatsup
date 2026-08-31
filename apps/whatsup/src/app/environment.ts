b4b34b30bb6baff00f3634893d709075ab105f8e
Marc <webdev@marcbakker.com>
2026-08-31 16:49:11 +0200

    toevoegen n8n url en translate url

--- apps/whatsup/src/app/environment.ts
+++ apps/whatsup/src/app/environment.ts
@@ -1,5 +1,7 @@
 interface AppConfigType {
   storageMethod: string;
+  n8n_webhook_url: string;
+  translateUrl: string;
   firebaseConfig?: {
     apiKey: string;
     authDomain: string;
@@ -13,6 +15,8 @@ interface AppConfigType {
 
 export const AppConfig: AppConfigType = {
   storageMethod: import.meta.env.NG_APP_STORAGE_METHOD,
+  n8n_webhook_url: import.meta.env['NG_APP_N8N_WEBHOOK_URL'],
+  translateUrl: 'https://api.mymemory.translated.net/get',
   firebaseConfig: {
     apiKey: import.meta.env.NG_APP_FIREBASE_API_KEY,
     authDomain: import.meta.env.NG_APP_FIREBASE_AUTH_DOMAIN,
@@ -21,5 +25,5 @@ export const AppConfig: AppConfigType = {
     messagingSenderId: import.meta.env.NG_APP_FIREBASE_MESSAGING_SENDER_ID,
     appId: import.meta.env.NG_APP_FIREBASE_APP_ID,
     measurementId: import.meta.env.NG_APP_FIREBASE_MEASUREMENT_ID,
-    }
+  },
 }
