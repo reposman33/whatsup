1ce0478ce1f48b6fbd491f91d2ee275bb191b42c
Marc <webdev@marcbakker.com>
2026-08-31 19:44:44 +0200

    Fix error "can't access property "NG_APP_N8N_WEBHOOK_URL", import.meta.env is undefined": verwijder property

--- apps/whatsup/src/app/environment.ts
+++ apps/whatsup/src/app/environment.ts
@@ -1,6 +1,5 @@
 interface AppConfigType {
   storageMethod: string;
-  n8n_webhook_url: string;
   translateUrl: string;
   firebaseConfig?: {
     apiKey: string;
@@ -15,7 +14,6 @@ interface AppConfigType {
 
 export const AppConfig: AppConfigType = {
   storageMethod: import.meta.env.NG_APP_STORAGE_METHOD,
-  n8n_webhook_url: import.meta.env.NG_APP_N8N_WEBHOOK_URL,
   translateUrl: 'https://api.mymemory.translated.net/get',
   firebaseConfig: {
     apiKey: import.meta.env.NG_APP_FIREBASE_API_KEY,
