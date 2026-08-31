8b3e422cfc6a689c7215b8ab2611b9b6acf865e1
Marc <webdev@marcbakker.com>
2026-08-31 16:50:22 +0200

    formatter de ISO dateTimestring

--- apps/whatsup/src/app/features/chat/chat/chat.ts
+++ apps/whatsup/src/app/features/chat/chat/chat.ts
@@ -1,11 +1,12 @@
 import { ChangeDetectionStrategy, Component, inject, input, ViewEncapsulation } from '@angular/core';
 import { Message } from '../../../models/message.model';
 import { AuthService } from '../../../features/auth/data-access/auth.service';
-import { UtilsService } from '../../../shared/services/utilsService';
+import { UtilsService } from '../../../shared/services/utils-service/utilsService';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'chat',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './chat.html',
   styleUrl: './chat.scss',
   encapsulation: ViewEncapsulation.Emulated,
